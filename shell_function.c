a70db2259d8ca235154a687e34f6af6a1a5a8c72
simowardi <daoist32@gmail.com>
2023-12-13 07:33:06 -0800

    update ets 4

--- shell_function.c
+++ shell_function.c
@@ -1,6 +1,54 @@
 #include "shell.h"
 
 /**
+ * tokenize_input - tokenize the command input into * * separate words
+ *@input: command input
+ *
+ *Return: the number of tokens
+ */
+
+char **tokenize_input(char *input)
+{
+	char *token = NULL, *tmp = NULL;
+	char **command = NULL;
+	int cpt = 0, i = 0;
+
+	if (!input)
+		return (NULL);
+	tmp = _strdup(input);
+	token = strtok(tmp, DELIM);
+
+	if (token == NULL)
+	{
+		free(input), input = NULL;
+		free(tmp), tmp = NULL;
+		return (NULL);
+	}
+	while (token)
+	{
+		cpt++;
+		token = strtok(NULL, DELIM);
+	}
+	free(tmp), tmp = NULL;
+
+	command = malloc(sizeof(char *) * (cpt + 1));
+	if (!command)
+	{
+		free(input), input = NULL;
+		return (NULL);
+	}
+	token = strtok(input, DELIM);
+	while (token)
+	{
+		command[i] = _strdup(token);
+		token = strtok(NULL, DELIM);
+		i++;
+	}
+	command[i] = NULL;
+	return (command);
+}
+
+/**
  * _execute - execute a command using fork and execve
  *@command: command text line
  *@argv: argument of function
@@ -31,49 +79,25 @@ int _execute(char **command, char **argv)
 }
 
 /**
- * tokenize_input - tokenize the command input into * * separate words
- *@input: command input
+ * freearray2D -  free array of strings
+ * @arr: 2d array of comand
  *
- *Return: the number of tokens
- */
+ **/
 
-char **tokenize_input(char *input)
+void freearray2D(char **arr)
 {
-	char *token = NULL, *tmp = NULL;
-	char **command = NULL;
-	int cpt = 0, i = 0;
+	int i;
 
-	if (!input)
-		return (NULL);
-	tmp = _strdup(input);
-	token = strtok(tmp, DELIM);
-	if (token == NULL)
+	if (!arr)
 	{
-		free(input), input = NULL;
-		free(tmp), tmp = NULL;
-		return (NULL);
+		return;
 	}
-	while (token)
+	for (i = 0; arr[i]; i++)
 	{
-		cpt++;
-		token = strtok(NULL, DELIM);
+		free(arr[i]);
+		arr[i] = NULL;
 	}
-	free(tmp), tmp = NULL;
-
-	command = malloc(sizeof(char *) * (cpt + 1));
-	if (!command)
-	{
-		free(input), input = NULL;
-		return (NULL);
-	}
-	token = strtok(input, DELIM);
-	while (token)
-	{
-		command[i] = _strdup(token);
-		token = strtok(NULL, DELIM);
-		i++;
-	}
-	command[i] = NULL;
-	return (command);
+	free(arr), arr = NULL;
 }
+
 
--- shell_mode.c
+++ shell_mode.c
@@ -9,33 +9,42 @@
 
 char *read_line(void)
 {
-		char *prompt = "$> ";
-		char *line = NULL;
-		size_t len = 0;
-		ssize_t n;
+	char *prompt = "$> ";
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
 
-		/* Check if input is from a terminal */
-		int is_interactive = isatty(STDIN_FILENO);
+	/* Check if input is from a terminal */
+	int is_interactive = isatty(STDIN_FILENO);
 
+	if (is_interactive)
+		write((STDOUT_FILENO), prompt, _strlen(prompt));
+
+	n = getline(&line, &len, stdin);
+
+	if (n == -1) /* end of file condition */
+	{
 		if (is_interactive)
-			write((STDOUT_FILENO), prompt, strlen(prompt));
+			write(STDOUT_FILENO, "\n", 1);
+		free(line);
+		return (NULL);
+	}
 
-		n = getline(&line, &len, stdin);
-
-		if (n == -1) /* end of file condition */
-		{
-			if (is_interactive)
-				write(STDOUT_FILENO, "\n", 1);
-			free(line);
-			return (NULL);
-		}
-		if (is_interactive)
-			/*Null-terminate the string for interactive mode */
+	if (is_interactive)
+	{
+		/*Null-terminate the string for interactive mode*/
+		if (line[n - 1] == '\n')
+			line[n - 1] = '\0'; /*(remove newline)*/
+	}
+	else
+	{
+		/*Null-terminate the string for non-interactive mode*/
+		if (line[n - 1] == '\n')
 			line[n - 1] = '\0';
 		else
-			/*Null-terminate the string for non-interactive mode*/
 			line[n] = '\0';
+	}
 
-		return (line);
+	return (line);
 }
 
