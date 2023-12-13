92d89e3c0114ab1832fa8d20e199451f1e55068b
simowardi <daoist32@gmail.com>
2023-12-13 13:26:57 -0800

    update 1&

--- shell_string_function.c
+++ shell_string_function.c
@@ -8,22 +8,22 @@
 char *_strdup(char *str)
 {
 	char *aaa;
-	int i, r = 0;
+	int i, len = 0;
 
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-
-	aaa = malloc(sizeof(char) * (i + 1));
-
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str = str - len;
+	aaa = malloc(sizeof(char) * (len + 1));
 	if (aaa == NULL)
 		return (NULL);
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
-
+	for (i = 0; i <= len; i++)
+		aaa[i] = str[i];
 	return (aaa);
 }
 
@@ -36,12 +36,79 @@ char *_strdup(char *str)
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int cmp;
+
+	cmp = (int)*s1 - (int)*s2;
+	while (*s1)
 	{
+		if (*s1 != *s2)
+			break;
 		s1++;
 		s2++;
+		cmp = (int)*s1 - (int)*s2;
 	}
-
-	return (*s1 - *s2);
+	return (cmp);
 }
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+		p++;
+
+	while (*src)
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = *src;
+	return (dest);
+}
+
+/**
+ * _strcpy - copies a string from source to destination
+ * @destination: the destinationination
+ * @source: the source
+ *
+ * Return: destination
+ */
+char *_strcpy(char *destination, char *source)
+{
+	int i = 0;
+
+	if (destination == source || source == 0)
+		return (destination);
+	while (source[i])
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = 0;
+	return (destination);
+}
+
 
